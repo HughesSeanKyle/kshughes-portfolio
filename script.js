454ed6c65b2e91a61bbe6770eae94b59b709f240
HughesSeanKyle <khughessean@yahoo.com>
2023-03-15 12:34:47 +0200

    Basic styling on projects and header animation

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Selectors
 const navBar = document.querySelector('.nav-bar');
 const skillsHeading = document.querySelector('.skills-heading');
+const projectHeading = document.querySelector('.project-heading');
+// const element = document.getElementById('thththt');
+// const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+
+// console.log('element', element);
+// console.log('elementPosition', elementPosition);
 
 // Event listeners
 document.addEventListener('scroll', () => {
@@ -13,9 +19,17 @@ document.addEventListener('scroll', () => {
 		navBar.style.boxShadow = 'none';
 	}
 
+	// Scroll animation for skills heading
 	if (scroll_position > 500) {
 		skillsHeading.classList.add('tracking-in-expand');
 	} else {
 		skillsHeading.classList.remove('tracking-in-expand');
+	}
+
+	// Scroll animation for Project heading
+	if (scroll_position > 4800) {
+		projectHeading.classList.add('tracking-in-expand');
+	} else {
+		projectHeading.classList.remove('tracking-in-expand');
 	}
 });
